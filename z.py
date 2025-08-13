afe1e3ae7d6d87f01ae07f453a70797f22d42d5c
Putinabillaa <aidiraputiabil@gmail.com>
2025-08-14 01:38:43 +0700

    chore: remove unused code

--- z.py
+++
@@ -1,5 +0,0 @@
-with open("data/labeled/for_echo_chamber_detection/indo_vaccination_labeled_136t_rawtext.csv", "r", encoding="utf-8-sig") as f:
-    content = f.read()
-
-with open("data/labeled/for_echo_chamber_detection/indo_vaccination_labeled_136t1.csv", "w", encoding="utf-8") as f:
-    f.write(content)
